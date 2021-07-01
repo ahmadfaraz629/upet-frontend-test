de5bf48ccc96fb3bb3f991d1f8e9110b922711c8
Ahmad Faraz <ahmadfaraz629@gmail.com>
2021-07-01 23:36:34 +0500

    Updates

---
+++ precache-manifest.2531ae66055c9ad779dfda6223edcb17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a599c34f884acd3fb43e26cb458cd4",
+    "url": "/upet-frontend-test/index.html"
+  },
+  {
+    "revision": "fd0af2247f43bbb83d34",
+    "url": "/upet-frontend-test/static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "20fc00af6237e11b4b4e",
+    "url": "/upet-frontend-test/static/js/2.cc933a4e.chunk.js"
+  },
+  {
+    "revision": "a5065ca4b8eff8c9ff57830ff71caa45",
+    "url": "/upet-frontend-test/static/js/2.cc933a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f9674119eb4bbe1eacc",
+    "url": "/upet-frontend-test/static/js/3.74317268.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/upet-frontend-test/static/js/3.74317268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e151c36c1ca8f6ccf533",
+    "url": "/upet-frontend-test/static/js/4.10a7884d.chunk.js"
+  },
+  {
+    "revision": "fd0af2247f43bbb83d34",
+    "url": "/upet-frontend-test/static/js/main.db977edc.chunk.js"
+  },
+  {
+    "revision": "cd42711057b3d1416d45",
+    "url": "/upet-frontend-test/static/js/runtime-main.c0667346.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a89fb87870fd3f1433889c382d1a771.js"
+  "/upet-frontend-test/precache-manifest.2531ae66055c9ad779dfda6223edcb17.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/upet-frontend-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
